6106297a786af94129b2e0d55fbd05a1947cb62d
“Vijay <vijaykumar2579@gmail.com>
2025-06-05 11:33:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import List from "./Components/List";
+
+const App = () => {
+  return (
+    <div className="container">
+      <span>This is our app</span>
+      <List />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/List.jsx
@@ -0,0 +1,5 @@
+const List = () => {
+  return <h2>This is Header</h2>;
+};
+
+export default List;
